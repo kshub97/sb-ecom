cd1882e6671f58d5a9c56ee0a9a482c136ad7227
shubham.kumar <shubham.k@clario.com>
2025-07-04 22:56:06 +0530

    Entity like Roles , user,Address Addded, Seller column to link user as seller in product ,cheetsheet updated with Mapped by ,@Jointable, Column FK,

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 @Entity(name = "products")
+@Table(name = "products")
+@ToString
 public class Product {
 
     @Id
@@ -25,4 +28,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name="category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
